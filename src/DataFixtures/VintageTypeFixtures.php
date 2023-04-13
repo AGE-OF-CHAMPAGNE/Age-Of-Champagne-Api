25645c45029253dc4a6d07db2329c850dd4a7c70
Dorynosaure <axel.rhul@gmail.com>
2023-04-13 12:50:53 +0200

    Ajout des fixtures pour VintageType

---
+++ src/DataFixtures/VintageTypeFixtures.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\VintageTypeFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class VintageTypeFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $file = file_get_contents(__DIR__.'/json/VintageType.json');
+        $array = json_decode($file, true);
+        VintageTypeFactory::createSequence($array);
+
+    }
+}
