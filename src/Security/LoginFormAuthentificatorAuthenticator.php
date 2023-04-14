6bcebbee082031c6180f257b69644efc1a09b8d8
Dorynosaure <axel.rhul@gmail.com>
2023-04-14 10:03:44 +0200

    Ajout d'une redirection à l'usage du /login

--- src/Security/LoginFormAuthentificatorAuthenticator.php
+++ src/Security/LoginFormAuthentificatorAuthenticator.php
@@ -46,9 +46,7 @@ class LoginFormAuthentificatorAuthenticator extends AbstractLoginFormAuthenticat
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('api_doc'));
     }
 
     protected function getLoginUrl(Request $request): string
