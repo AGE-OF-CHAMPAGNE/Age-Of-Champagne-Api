c5dfb847812f972efd9fc900fd0b495b9ed3dbf2
Dorynosaure <axel.rhul@gmail.com>
2023-04-14 11:45:16 +0200

    Correction du fait que l'api ne revoyait pas d'erreur 400*

--- src/Security/LoginFormAuthentificatorAuthenticator.php
+++ src/Security/LoginFormAuthentificatorAuthenticator.php
@@ -5,8 +5,10 @@ namespace App\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
@@ -53,4 +55,24 @@ class LoginFormAuthentificatorAuthenticator extends AbstractLoginFormAuthenticat
     {
         return $this->urlGenerator->generate(self::LOGIN_ROUTE);
     }
+
+    /**
+     * Override to control what happens when the user hits a secure page
+     * but isn't logged in yet.
+     *
+     * @throws HttpException
+     */
+    public function start(Request $request, AuthenticationException $authException = null): RedirectResponse
+    {
+        /** @var string $route current route */
+        $route = $request->get('_route');
+        // API route ?
+        if (str_starts_with($route, '_api_')) {
+            throw new HttpException(Response::HTTP_UNAUTHORIZED);
+        }
+        $url = $this->getLoginUrl($request);
+
+        return new RedirectResponse($url);
+    }
+
 }
