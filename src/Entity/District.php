28bd3b9bb06c48cba6013af4c35c85570a4c90ff
Dorynosaure <axel.rhul@gmail.com>
2023-04-17 10:25:54 +0200

    Ajout de l'API pour les regions

--- src/Entity/District.php
+++ src/Entity/District.php
@@ -2,11 +2,48 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\DistrictRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ApiResource(
+    operations: [
+        new Get(
+            uriTemplate: '/district/{id}',
+            openapiContext: [
+                'summary' => 'Retrieves a District',
+                'description' => 'Retrieves a District',
+                'responses' => [
+                    '200' => [
+                        'description' => 'District found',
+                    ],
+                    '404' => [
+                        'description' => 'District not found',
+                    ],
+                ],
+            ]
+        ),
+        new GetCollection(
+            openapiContext: [
+                'summary' => 'Retrieves a District collection',
+                'description' => 'Retrieves a District collection',
+                'responses' => [
+                    '200' => [
+                        'description' => 'District collection found',
+                    ],
+                    '404' => [
+                        'description' => 'District collection not found',
+                    ],
+                ],
+            ]
+        ),
+    ]
+)]
 #[ORM\Entity(repositoryClass: DistrictRepository::class)]
 class District
 {
@@ -15,15 +52,42 @@ class District
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ApiProperty(
+        openapiContext: [
+            'type' => 'string',
+            'example' => 'Champenoise',
+        ]
+    )]
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    #[ApiProperty(
+        openapiContext: [
+            'type' => 'float',
+            'example' => '49.3',
+        ]
+    )]
     #[ORM\Column(nullable: true)]
     private ?float $size = null;
 
+    #[ApiProperty(
+        openapiContext: [
+            'type' => 'string',
+            'example' => 'D79897',
+        ]
+    )]
     #[ORM\Column(length: 255)]
     private ?string $color_code = null;
 
+    #[ApiProperty(
+        openapiContext: [
+            'type' => 'array path',
+            'example' => '[
+            "/api/vintages/2",
+            "/api/vintages/4"
+            ]',
+        ]
+    )]
     #[ORM\OneToMany(mappedBy: 'district', targetEntity: Vintage::class)]
     private Collection $vintages;
 
