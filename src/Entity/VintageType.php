8bc1387718db3064e14507a01d9e1bfd1245b7aa
Dorynosaure <axel.rhul@gmail.com>
2023-04-17 10:26:53 +0200

    Ajout de l'API pour les types de crus

--- src/Entity/VintageType.php
+++ src/Entity/VintageType.php
@@ -2,11 +2,49 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\VintageTypeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
+#[ApiResource(
+    operations: [
+        new Get(
+            uriTemplate: '/vintage_type/{id}',
+            openapiContext: [
+                'summary' => 'Retrieves a Vintage Type',
+                'description' => 'Retrieves a Vintage Type',
+                'responses' => [
+                    '200' => [
+                        'description' => 'Vintage Type found',
+                    ],
+                    '404' => [
+                        'description' => 'Vintage Type not found',
+                    ],
+                ],
+            ]
+        ),
+        new GetCollection(
+            openapiContext: [
+                'summary' => 'Retrieves a Vintage Type collection',
+                'description' => 'Retrieves a Vintage Type collection',
+                'responses' => [
+                    '200' => [
+                        'description' => 'Vintage Type collection found',
+                    ],
+                    '404' => [
+                        'description' => 'Vintage Type collection not found',
+                    ],
+                ],
+            ]
+        ),
+    ]
+)]
 #[ORM\Entity(repositoryClass: VintageTypeRepository::class)]
 class VintageType
 {
@@ -15,9 +53,25 @@ class VintageType
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Groups(['get_User', 'set_User'])]
+    #[ApiProperty(
+        openapiContext: [
+            'type' => 'string',
+            'example' => 'Grand Cru',
+        ]
+    )]
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    #[ApiProperty(
+        openapiContext: [
+            'type' => 'array path',
+            'example' => '[
+            "/api/vintage/2",
+            "/api/vintage/4"
+            ]',
+        ]
+    )]
     #[ORM\OneToMany(mappedBy: 'vintage_type', targetEntity: Vintage::class)]
     private Collection $vintages;
 
