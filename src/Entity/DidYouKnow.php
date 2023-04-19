4964c3bd36bac453714c8cc2f74f67bb8376b2a1
Dorynosaure <axel.rhul@gmail.com>
2023-04-19 16:07:09 +0200

    Ajout de l'API

--- src/Entity/DidYouKnow.php
+++ src/Entity/DidYouKnow.php
@@ -3,12 +3,16 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\DidYouKnowRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DidYouKnowRepository::class)]
 #[ApiResource]
+#[Get]
+#[GetCollection]
 class DidYouKnow
 {
     #[ORM\Id]
