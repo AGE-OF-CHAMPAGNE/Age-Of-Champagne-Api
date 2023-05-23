07dfe2c9d04262314cf684d2a1c2fe27d996294d
rhul0001 <axel.rhul@etudiant.univ-reims.fr>
2023-05-23 10:49:01 +0200

    Ajout de EasyAdmin

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
