d78b3b93c38ee12afb92202231230bd8f40fc5cb
rhul0001 <axel.rhul@etudiant.univ-reims.fr>
2023-05-23 15:36:20 +0200

    Ajout du crud pour le user

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureAssets(Assets $assets): Assets
+    {
+        return $assets->addCssFile('https://fonts.googleapis.com/icon?family=Material+Icons');
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id', 'Identifiant')->onlyOnIndex(),
+            EmailField::new('email', 'Adresse eMail'),
+            ArrayField::new('roles', 'Roles'),
+            FormField::addPanel('User'),
+            TextField::new('firstName', 'Prénom'),
+            TextField::new('lastName', 'Nom de famille'),
+            BooleanField::new('wantSeeDYK', 'Affichage des pop-up'),
+            AssociationField::new('Vintages')
+                ->setFormTypeOption('choice_label', 'name'),
+        ];
+    }
+}
