276d4ee25460f0df98f573afdb8038659dab3893
rhul0001 <axel.rhul@etudiant.univ-reims.fr>
2023-05-24 13:57:35 +0200

    Affichage de stats

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Benefit;
+use App\Entity\User;
+use App\Entity\Vintage;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
@@ -17,10 +21,15 @@ use Symfony\Component\Security\Core\User\UserInterface;
  */
 class DashboardController extends AbstractDashboardController
 {
+    // Permet d'avoit acces au fonction de getRepository
+    // public function __construct(private EntityManagerInterface $entityManager){}
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig');
+        // compte le nombre de user dans le site
+        // $user = count($this->entityManager->getRepository(User::class)->findAll());
+        return $this->render('admin/index.html.twig'/* ,['user' => $user] */);
     }
 
     public function configureDashboard(): Dashboard
@@ -31,10 +40,17 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::section('Avantage', 'fa fa-tags');
-        yield MenuItem::linkToCrud('Liste des Avantages', 'fa', Benefit::class);
-        yield MenuItem::linkToCrud('Ajouter un Avantage', 'fa', Benefit::class)
+        yield MenuItem::linkToDashboard("Page d'acceuil", 'fa fa-home');
+        yield MenuItem::section('Crus', 'fa fa-wine-bottle');
+        yield MenuItem::linkToCrud('Liste des Crus', 'fa fa-list', Vintage::class);
+
+        yield MenuItem::section('Utilisateurs', 'fa fa-user');
+        yield MenuItem::linkToCrud('Liste des Utilisateurs', 'fa fa-list', User::class);
+        yield MenuItem::linkToCrud('Ajouter un Utilisateur', 'fa fa-plus', User::class)
+            ->setAction('new');
+        yield MenuItem::section('Avantages', 'fa fa-tags');
+        yield MenuItem::linkToCrud('Liste des Avantages', 'fa fa-list', Benefit::class);
+        yield MenuItem::linkToCrud('Ajouter un Avantage', 'fa fa-plus', Benefit::class)
             ->setAction('new');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
@@ -42,7 +58,13 @@ class DashboardController extends AbstractDashboardController
     public function configureUserMenu(UserInterface $user): UserMenu
     {
         return parent::configureUserMenu($user)
-            // use the given $user object to get the user name
             ->setName("{$user->getFirstName()} {$user->getLastName()}");
+    }
+
+    public function configureCrud(): Crud
+    {
+        return Crud::new()
+            ->setDefaultSort(['id' => 'ASC'])
+        ;
     }
 }
