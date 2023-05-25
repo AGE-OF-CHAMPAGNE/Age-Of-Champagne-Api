f1e2e1b87c580a775e8447f4b81c0e99432d336b
rhul0001 <axel.rhul@etudiant.univ-reims.fr>
2023-05-25 14:18:02 +0200

    Ajout du crud pour le saviez vous

---
+++ src/Controller/Admin/DidYouKnowCrudController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\DidYouKnow;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class DidYouKnowCrudController extends AbstractCrudController
+{
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPageTitle('index', 'Le saviez vous ?')
+            ->setEntityLabelInSingular('Le saviez vous ?');
+    }
+
+    public static function getEntityFqcn(): string
+    {
+        return DidYouKnow::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id', 'Identifiant'),
+            TextField::new('description'),
+        ];
+    }
+}
