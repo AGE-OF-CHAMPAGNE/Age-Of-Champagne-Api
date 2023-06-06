d2031694a9fe2543a573b63575d0685083567162
rhul0001 <axel.rhul@etudiant.univ-reims.fr>
2023-06-06 10:24:37 +0200

    Import is granted

--- src/Controller/AdminDeleteVintageCardController.php
+++ src/Controller/AdminDeleteVintageCardController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Vintage;
 use App\Repository\VintageRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
--- src/Controller/AdminVintageController.php
+++ src/Controller/AdminVintageController.php
@@ -6,6 +6,7 @@ use App\Entity\Vintage;
 use App\Form\VintageTypeFormType;
 use App\Repository\VintageRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
